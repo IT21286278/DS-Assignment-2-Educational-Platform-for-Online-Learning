afd0e29a5f69500aee4778e64ba8251f9f399aaa
Chamikara Vithanage <it21219498@my.sliit.lk>
2024-04-28 03:32:50 +0530

    Implement user authentication and authorization functionality

---
+++ user-service/models/User.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const UserModel = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    max: 50,
+    unique: true,
+
+    required: [true, "email is required"],
+  },
+
+  password: {
+    type: String,
+    required: [true, "password is required"],
+  },
+
+  role: {
+    type: String,
+    enum: ["Student", "Faculty", "Admin", "Lecturer"],
+    default: "Student",
+  },
+});
+
+const User = new mongoose.model("User", UserModel);
+
+export default User;
