6601279859926a72669d668e135fca456aa794ae
Chamikara Vithanage <it21219498@my.sliit.lk>
2024-04-28 00:42:10 +0530

    Gatway service implemented

--- gateway/app.js
+++ gateway/app.js
@@ -2,9 +2,9 @@ import express from "express";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import cors from "cors";
-import { connect } from "./config/db_con.js";
+import proxy from "express-http-proxy";
 
-dotenv.config({ path: "./config/.env" });
+dotenv.config();
 
 const app = express();
 
@@ -13,17 +13,15 @@ app.use(express.json()); //Send respones in json fomrat
 app.use(morgan("tiny")); //log requests
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello from express");
-});
+app.use("/course", proxy("http://localhost:8001"));
+app.use("/user", proxy("http://localhost:8002"));
 
 //server config
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, async () => {
   try {
-    await connect();
-    console.log(`Sever is running on port ${PORT}`);
+    console.log(`Gateway is running on port ${PORT}`);
   } catch (error) {
-    console.log(err);
+    console.log(error);
   }
 });
