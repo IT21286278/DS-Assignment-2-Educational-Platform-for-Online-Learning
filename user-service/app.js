fc657d162ee7e246a91c08cb44c08319ae30496e
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-05-10 23:08:32 +0530

    Update dependencies in user-service

--- user-service/app.js
+++ user-service/app.js
@@ -1,8 +1,9 @@
-import express from "express";
-import morgan from "morgan";
-import dotenv from "dotenv";
-import cors from "cors";
-import { connect } from "./config/db_con.js";
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connect } from './config/db_con.js';
+import userRouter from './routes/auth.js';
 
 dotenv.config();
 
@@ -10,13 +11,11 @@ const app = express();
 
 //middlewares
 app.use(express.json()); //Send respones in json fomrat
-app.use(morgan("tiny")); //log requests
+app.use(morgan('tiny')); //log requests
 app.use(cors());
 
 //routes
-app.use("/", (req, res, next) => {
-  return res.status(200).json({ message: "User service is running" });
-});
+app.use('/user', userRouter);
 
 //server config
 const PORT = process.env.PORT || 9000;
