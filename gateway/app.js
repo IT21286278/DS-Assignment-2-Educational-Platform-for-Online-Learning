d360aae0de256fd7a99350998ae744408de66eab
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-05-10 23:07:52 +0530

    Gateway updated

--- gateway/app.js
+++ gateway/app.js
@@ -1,8 +1,8 @@
-import express from "express";
-import morgan from "morgan";
-import dotenv from "dotenv";
-import cors from "cors";
-import proxy from "express-http-proxy";
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import proxy from 'express-http-proxy';
 
 dotenv.config();
 
@@ -10,11 +10,12 @@ const app = express();
 
 //middlewares
 app.use(express.json()); //Send respones in json fomrat
-app.use(morgan("tiny")); //log requests
+app.use(morgan('tiny')); //log requests
 app.use(cors());
 
-app.use("/course", proxy("http://localhost:8001"));
-app.use("/user", proxy("http://localhost:8002"));
+app.use('/course', proxy('http://localhost:8001'));
+app.use('/user', proxy('http://localhost:8002'));
+app.use('/enrollment', proxy('http://localhost:8003'));
 
 //server config
 const PORT = process.env.PORT || 9000;
