6becc9fdfefbe058de236758aa105419ab54cc7a
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-05-12 02:51:48 +0530

    Dockerize notification-service component

--- notification-service/app.js
+++ notification-service/app.js
@@ -1,9 +1,9 @@
-import express from "express";
-import morgan from "morgan";
-import dotenv from "dotenv";
-import cors from "cors";
-// import { connect } from "./config/db_con.js";
-import nodemailer from "nodemailer"; // Import nodemailer module
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connect } from './config/db_con.js';
+import nodemailer from 'nodemailer'; // Import nodemailer module
 
 dotenv.config();
 
@@ -11,7 +11,7 @@ dotenv.config();
 const sendTimetableNotification = async (studentEmails, subject, message) => {
   // Create a nodemailer transporter
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    service: 'gmail',
     auth: {
       user: process.env.USER,
       pass: process.env.PASSWORD,
@@ -30,9 +30,9 @@ const sendTimetableNotification = async (studentEmails, subject, message) => {
 
     try {
       await transporter.sendMail(mailOptions);
-      console.log("Email sent successfully to:", email);
+      console.log('Email sent successfully to:', email);
     } catch (err) {
-      console.log("Error sending email", err);
+      console.log('Error sending email', err);
     }
   }
 };
@@ -41,31 +41,31 @@ const app = express();
 
 //middlewares
 app.use(express.json()); //Send responses in json format
-app.use(morgan("tiny")); //log requests
+app.use(morgan('tiny')); //log requests
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello from express");
+app.get('/', (req, res) => {
+  res.send('Hello from express');
 });
 
 // Route to send notification
-app.post("/send-notification", async (req, res) => {
+app.post('/send-notification', async (req, res) => {
   const { studentEmails, subject, message } = req.body; // Extract data from request body
 
   if (!studentEmails || !subject || !message) {
     return res
       .status(400)
       .send(
-        "Please provide studentEmails, subject, and message in the request body."
+        'Please provide studentEmails, subject, and message in the request body.'
       );
   }
 
   try {
     await sendTimetableNotification(studentEmails, subject, message);
-    res.send("Notification sent successfully.");
+    res.send('Notification sent successfully.');
   } catch (error) {
     console.log(error);
-    res.status(500).send("Error sending notification.");
+    res.status(500).send('Error sending notification.');
   }
 });
 
@@ -73,7 +73,7 @@ app.post("/send-notification", async (req, res) => {
 const PORT = process.env.PORT || 9000;
 app.listen(PORT, async () => {
   try {
-    // await connect();
+    await connect();
     console.log(`Notification service is running on port ${PORT}`);
   } catch (error) {
     console.log(error);
