2d397e511625934507bd68d048b62bf433e57d55
Chamikara Vithanage <it21219498@my.sliit.lk>
2024-05-12 13:19:46 +0530

    chore: Add Cancel and Success components with CSS styles

--- frontend/src/components/Cancel.js
+++ frontend/src/components/Cancel.js
@@ -1,5 +1,21 @@
+import React from "react";
+import { Cancel as CancelIcon } from "@mui/icons-material";
+import "./Cancel.css";
+
 function Cancel() {
-  return <h1>Sorry to see you cancelled your Stripe payment!</h1>;
+  return (
+    <div className='cancel-container'>
+      <div className='cancel-content'>
+        <CancelIcon className='cancel-icon' />
+        <h1 className='cancel-heading'>
+          Sorry to see you cancelled your Stripe payment!
+        </h1>
+        <p className='cancel-message'>
+          If you have any questions or concerns, please feel free to contact us.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Cancel;
--- frontend/src/components/Success.js
+++ frontend/src/components/Success.js
@@ -1,5 +1,19 @@
+import React from "react";
+import TaskAltTwoToneIcon from "@mui/icons-material/TaskAltTwoTone";
+import "./Success.css";
+
 function Success() {
-  return <h1>Thank you for your purchase!</h1>;
+  return (
+    <div className='success-container'>
+      <div className='success-content'>
+        <TaskAltTwoToneIcon className='success-icon' />
+        <h1 className='success-heading'>Thank you for your purchase!</h1>
+        <p className='success-message'>
+          Your course purchase was successful. Start learning now!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Success;
