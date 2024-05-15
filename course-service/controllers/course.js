3e865f74a94cadc3c5ad0fce7474ee5e7cc81ca2
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2024-05-15 23:09:52 +0530

    feat: Add fetchAllDraftCourses endpoint

--- course-service/controllers/course.js
+++ course-service/controllers/course.js
@@ -228,6 +228,18 @@ export const getCourseWithCompany = async (req, res) => {
   }
 };
 
+export const fetchAllDraftCourses = async (req, res) => {
+  try {
+    const courses = await Course.find({ status: "draft" }).populate(
+      "company",
+      "-description -status"
+    );
+    res.status(200).json({ courses });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
 export const addContent = async (req, res) => {
   try {
     const { note, video, quiz, courseId, type, status, order } = req.body;
