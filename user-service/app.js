ed7f1b0416abb5921b1f2db331fe7d3d03176583
Ashen_IT21286278 <99505510+IT21286278@users.noreply.github.com>
2024-05-16 03:57:39 +0530

    Gateway component updated

--- user-service/app.js
+++ user-service/app.js
@@ -1,20 +1,20 @@
-import express from "express";
-import morgan from "morgan";
-import dotenv from "dotenv";
-import cors from "cors";
-import { connect } from "./config/db_con.js";
-import authRouter from "./routes/auth.js";
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import { connect } from './config/db_con.js';
+import authRouter from './routes/auth.js';
 
 dotenv.config();
 const app = express();
 
 //middlewares
 app.use(express.json()); //Send respones in json fomrat
-app.use(morgan("tiny")); //log requests
+app.use(morgan('tiny')); //log requests
 app.use(cors());
 
 //routes
-app.use("/api", authRouter);
+app.use('/', authRouter);
 
 //server config
 const PORT = process.env.PORT || 9000;
